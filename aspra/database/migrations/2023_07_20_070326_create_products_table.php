bd1686d9292ce3a331a99e766ef6b68bee88b3c5
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-07-20 14:13:22 +0700

    create products migration

---
+++ aspra/database/migrations/2023_07_20_070326_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->text('bahan');
+            $table->text('berat');
+            $table->text('volume');
+            $table->text('warna');
+            $table->text('packing');
+            $table->text('isi_produk');
+            $table->text('jenis_test');
+            $table->text('outstanding')->nullable();
+            $table->text('kebutuhan_per_bulan')->nullable();
+            $table->date('pengajuan_terakhir')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
