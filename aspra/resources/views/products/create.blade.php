64d0bebffdb027752864466d09f9e73c6316011c
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-07-29 20:24:35 +0700

    create new template with sidebar

--- aspra/resources/views/products/create.blade.php
+++ aspra/resources/views/products/create.blade.php
@@ -1,20 +1,20 @@
-<x-app-layout>
+<x-custom-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Products') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="pull-right">
+                <a class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" href="{{ route('products.index') }}"> Back</a>
+            </div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="row">
                     <div class="col-lg-12 margin-tb">
                         <div class="pull-left">
                             <h2>Add New Product</h2>
-                        </div>
-                        <div class="pull-right">
-                            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
                         </div>
                     </div>
                 </div>
--- aspra/resources/views/products/index.blade.php
+++ aspra/resources/views/products/index.blade.php
@@ -1,11 +1,11 @@
-<x-app-layout>
+<x-custom-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Products') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-8">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="row">
@@ -15,7 +15,7 @@
                         </div>
                         <div class="pull-right">
                             @can('product-create')
-                                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+                                <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="{{ route('products.create') }}"> Create New Product</a>
                             @endcan
                         </div>
                     </div>
