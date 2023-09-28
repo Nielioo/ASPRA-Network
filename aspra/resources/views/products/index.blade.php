c9a9ca61b96990025438565ba34f48df355ffced
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-09-28 08:54:54 +0700

    fix: product.index ui

--- aspra/resources/views/products/index.blade.php
+++ aspra/resources/views/products/index.blade.php
@@ -41,9 +41,9 @@
                     </div>
                 @endif
 
-                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <div class="relative overflow-x-auto shadow-sm sm:rounded-lg">
+                    <table class="w-full border border-gray-200 text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">No</th>
                                 <th scope="col" class="px-6 py-3">Kode Produk</th>
@@ -85,7 +85,9 @@
                     </table>
                 </div>
 
-                {!! $products->links() !!}
+                <div class="pt-5">
+                    {!! $products->links() !!}
+                </div>
 
             </div>
         </div>
