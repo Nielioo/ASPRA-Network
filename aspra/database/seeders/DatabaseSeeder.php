bc0c1b552636501787c41d1660acd837f98755a3
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-14 11:22:59 +0700

    feat: fix database seeder

--- aspra/database/seeders/DatabaseSeeder.php
+++ aspra/database/seeders/DatabaseSeeder.php
@@ -14,19 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        // DB::table('users')->insert([
-        //     'name' => 'Daniel Aprillio',
-        //     'email' => 'daniel@gmail.com',
-        //     'password' => Hash::make('12345678'),
-        //     'created_at' => now(),
-        //     'updated_at' => now(),
-        // ]);
+        $this->call(
+            [
+                PermissionTableSeeder::class,
+                CreateAdminUserSeeder::class,
+            ]
+        );
     }
 }
