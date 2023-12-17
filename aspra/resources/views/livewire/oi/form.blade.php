5a1016b5db55847ad3fc5301bc6f78adb7995167
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-12-17 21:02:37 +0700

    feat: minor docs

--- aspra/resources/views/livewire/oi/form.blade.php
+++ aspra/resources/views/livewire/oi/form.blade.php
@@ -1,3 +1,4 @@
+{{-- Form OI --}}
 @if ($errors->any())
     <div class="px-4 py-2">
         <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
