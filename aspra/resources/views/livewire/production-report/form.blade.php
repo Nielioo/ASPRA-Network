bac62820088943fdbcd3c81e1a9a696f1963ddc8
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2024-04-30 08:20:00 +0700

    feat: update schedule search bar (more detail)

--- aspra/resources/views/livewire/production-report/form.blade.php
+++ aspra/resources/views/livewire/production-report/form.blade.php
@@ -21,17 +21,20 @@
         {{-- schedule search bar --}}
         <div class="px-4 py-2">
             <x-label for="schedule" value="{{ __('Pilih Jadwal') }}" />
-            <p class="p-2 border border-gray-300 rounded-none rounded-t-lg w-full">{{$selectedSchedule ? $selectedSchedule->product_name : '-'}}</p>
+            @if ($selectedSchedule)
+                <p class="p-2 border border-gray-300 rounded-none rounded-t-lg w-full">
+                    [{{ $selectedSchedule->id }}] - ({{ $selectedSchedule->date_start }}
+                    {{ $selectedSchedule->shift_start }} s.d. {{ $selectedSchedule->date_end }}
+                    {{ $selectedSchedule->shift_end }}) - {{ $selectedSchedule->product_name }}
+                </p>
+            @else
+                <p class="text-slate-400 p-2 border border-gray-300 rounded-none rounded-t-lg w-full">No schedule
+                    selected. Please select schedule first.</p>
+            @endif
             <x-input type="text"
                 class="border border-gray-300 rounded-none rounded-b-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="search schedules here..." wire:model="query"
-                wire:keydown.escape="resetVars" wire:click.outside="resetVars" />
-            <div class="overflow-y-scroll bg-white w-full" wire:loading>
-                <ul class="list-outside list-none">
-                    <li class="p-1 border border-slate-200 cursor-pointer divide-y">
-                        Searching...</li>
-                </ul>
-            </div>
+                placeholder="search schedules here..." wire:model="query" wire:keydown.escape="resetVars"
+                wire:click.outside="resetVars" />
             <div class="overflow-y-scroll bg-white w-full">
                 @if (!empty($query))
                     <div class="fixed top-0 right-0 bottom-0 left-0 -z-10" wire:click="resetVars"></div>
@@ -40,12 +43,15 @@
                             <ul class="list-outside list-none">
                                 <li class="p-2 border-x-2 border-b-2 border-slate-200 cursor-pointer divide-y hover:bg-slate-100"
                                     wire:click="updateSelectedSchedule({{ $schedule['id'] }})">
-                                    {{ $schedule['product_name'] }}</li>
+                                    [{{ $schedule['id'] }}] - ({{ $schedule['date_start'] }}
+                                    {{ $schedule['shift_start'] }} s.d. {{ $schedule['date_end'] }}
+                                    {{ $schedule['shift_end'] }}) - {{ $schedule['product_name'] }}
+                                </li>
                             </ul>
                         @endforeach
                     @else
                         <ul class="list-outside list-none">
-                            <li class="p-1 border border-slate-200 cursor-pointer divide-y hover:bg-slate-100">
+                            <li class="p-2 border-x-2 border-b-2 border-slate-200 cursor-pointer divide-y hover:bg-slate-100">
                                 No result found
                             </li>
                         </ul>
@@ -83,7 +89,8 @@
                         </div>
                         <div class="text-md p-2 font-bold">Mesin yang digunakan</div>
                         <div class="grid-cols-subgrid col-span-2">
-                            <div class="text-md p-2">[{{ $selectedSchedule->machine->id }}] - {{ $selectedSchedule->machine->name }}
+                            <div class="text-md p-2">[{{ $selectedSchedule->machine->id }}] -
+                                {{ $selectedSchedule->machine->name }}
                             </div>
                         </div>
                         <div class="text-md p-2 font-bold">Nama Produk yang ingin di produksi</div>
@@ -98,11 +105,13 @@
                         </div>
                         <div class="text-md p-2 font-bold">Jadwal mulai produksi</div>
                         <div class="grid-cols-subgrid col-span-2">
-                            <div class="text-md p-2">{{ $selectedSchedule->date_start }} ({{ $selectedSchedule->shift_start }})</div>
+                            <div class="text-md p-2">{{ $selectedSchedule->date_start }}
+                                ({{ $selectedSchedule->shift_start }})</div>
                         </div>
                         <div class="text-md p-2 font-bold">Jadwal selesai produksi</div>
                         <div class="grid-cols-subgrid col-span-2">
-                            <div class="text-md p-2">{{ $selectedSchedule->date_end }} ({{ $selectedSchedule->shift_end }})</div>
+                            <div class="text-md p-2">{{ $selectedSchedule->date_end }}
+                                ({{ $selectedSchedule->shift_end }})</div>
                         </div>
                         <div class="text-md p-2 font-bold">Output STD / Shift</div>
                         <div class="grid-cols-subgrid col-span-2">
