04dc8d3e4387992ab89b4cf05d64c98a4fd3d2b5
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2024-05-04 17:13:13 +0700

    feat: whatsapp notification succeed

--- aspra/app/Http/Controllers/WhatsappController.php
+++ aspra/app/Http/Controllers/WhatsappController.php
@@ -5,29 +5,55 @@ namespace App\Http\Controllers;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
+// https://nanonets.com/blog/use-whatsapp-api-to-send-messages/
+
 class WhatsAppController extends Controller
 {
     public function sendMessage(Request $request)
     {
         $client = new Client();
+        $token = 'Bearer EAANKo9YdU28BO6TUMN4FlJTUmvYtRB2Ws6fhdjFlBtVTzTSO9jt5DXP54hOlCT2gEK6jXhNJoIqDWFXzkHce7rDj83sj3hk1ZCkbwZB6AUlRUZCzNV2GUpkhJmvSC6oVESgujZBYPbbrXpot4XQJxaZA9KDuy2yzSx4IjreZCZACpL12CWQNknEoZBVr5P6cL5Mb';
 
         $response = $client->post('https://graph.facebook.com/v19.0/260819393788777/messages', [
             'headers' => [
-                'Authorization' => 'Bearer EAANKo9YdU28BO4LH3DpGPCUqTO2HjbCmV2FRYxjzMqHCMESNfAaUuTqU125FsxYUWBbFLGcxxPlQpJHXxGLHEenoZAcBEUZBn8ZA6xVivnjFeSjU4FdNb2JjflN9ltxPJEjHs66LmYZA31PJsZAaQdiJ3UNlQFfMR01SSINgtwLkdtwDAZBkvLUz4j16m5qlVwtxd5rFZCBJCIRaZAMtiycZD',
+                'Authorization' => $token,
                 'Content-Type' => 'application/json'
             ],
             'json' => [
                 'messaging_product' => 'whatsapp',
+                'recipient_type' => 'individual',
                 'to' => '6285105118880',
+
                 'type' => 'template',
                 'template' => [
-                    'name' => 'hello_world',
+                    'name' => 'production_reject_notifications',
                     'language' => [
                         'code' => 'en_US'
                     ],
-                ]
-            ]
+                    'components' => [
+                        [
+                            'type' =>'body',
+                            'parameters' => [
+                                [
+                                    'type' => 'text',
+                                    'text' => 'PRODUCT_NAME'
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => 'REJECT_PERCENTAGE'
+                                ],
+                                [
+                                    'type' => 'text',
+                                    'text' => 'DATE SHIFT'
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
         ]);
+
+        // dd($response->getBody()->getContents());
 
         return back();
     }
