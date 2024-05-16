7537eb881c9eeac8d26615a7ac2166967e1f66a3
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2024-05-16 11:27:24 +0700

    fix: table UI position when null

--- resources/views/livewire/product/product-table.blade.php
+++ resources/views/livewire/product/product-table.blade.php
@@ -81,7 +81,7 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4" class="px-6 py-4 text-center">No product has been made.
+                        <td colspan="8" class="px-6 py-4 text-center">No product has been made.
                         </td>
                     </tr>
                 @endforelse
